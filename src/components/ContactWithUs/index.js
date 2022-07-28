74dcf97c3b52b3a2db6d3fdc375f36d873becf32
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-07-28 12:21:12 +0500

    master

--- src/components/ContactWithUs/index.js
+++ src/components/ContactWithUs/index.js
@@ -7,7 +7,7 @@ const ContactWithUs = () => {
    return (
       <div className="container px-5">
          <div className="row">
-            <div className="col-11 offset-1">
+            <div className="col-xl-11 offset-xl-1 col-12">
                <div className="row align-items-center justify-content-center gap-3">
                   <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                      <TitleH1 title={t("checkoutCourse")} />
--- src/styles/index.js
+++ src/styles/index.js
@@ -151,3 +151,27 @@ border: 1px solid #E5E5EA;
 box-shadow: 0px 12px 16px -4px rgba(16, 24, 40, 0.1), 0px 4px 6px -2px rgba(16, 24, 40, 0.05);
 border-radius: 16px;
 `
+
+export const StickCardCourseDetailWrapper = styled.div`
+border: 1px solid;
+/* position: sticky; */
+top: 30px;
+border: 1.5px solid #E5E5EA;
+border-radius: 16px;
+a{
+  display: block;
+  text-decoration: none;
+  font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #1C1C1E !important;
+padding: 10px 20px;
+
+  &.active-link{
+    color: red;
+  }
+  &:hover{
+    background-color: #EBEBF599;
+  }
+}
+`
