d81a87dc511226d0521c9c44095c571ec2d279b3
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-07-29 07:15:19 +0500

    master

--- src/pages/Experts/ExpertDetail/index.js
+++ src/pages/Experts/ExpertDetail/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Spin } from 'antd'
-import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import CarouselSlider from '../../../components/Carousel'
 import { instance } from '../../../redux/actions'
