3175fc391725d451a32c43afb997c53e620b0e9e
TurabekIrgashev <turabekirgashev2018@gmail.com>
2022-07-30 03:32:06 +0500

    master

--- src/components/Employees/index.jsx
+++ src/components/Employees/index.jsx
@@ -10,6 +10,7 @@ import { instance } from "../../redux/actions";
 import { DefaultCardWrapper, StickCardCourseDetailWrapper } from "../../styles";
 import DefaultButton from "../DefaultButton";
 import iconka from "../../assets/icons/searchIcon.svg";
+import DefaultExpertCard from "../DefaultCardExperts";
 
 const Employees = () => {
   const { id } = useParams();
@@ -54,10 +55,10 @@ const Employees = () => {
   return (
     <Spin spinning={loading}>
       <div className="container ">
-        <div className="row">
-          <div className="mt-4 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+        <div className='row'>
+          <div className="mt-4 col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
             <StickCardCourseDetailWrapper
-              className="mt-5 ml-5 position-sticky "
+              className="mt-5 position-sticky "
               style={{ padding: "20px" }}
             >
               <h5 className="mt-3 mb-3">Кадры</h5>
@@ -78,52 +79,20 @@ const Employees = () => {
                   aria-label="Text input with radio button"
                   placeholder="Запишитесь сейчас"
                   style={{ borderLeft: "none" }}
-                  onChange={(e) => setSearch(e.target.value)}
                 />
               </div>
               <ul>
                 {courses.map((e, i) => (
-                  <li className="my-3 hover">{e.courses[0].name}</li>
+                  <li className="my-3">{e.courses[0].name}</li>
                 ))}
               </ul>
             </StickCardCourseDetailWrapper>
           </div>
-          <div className="mt-4 col-xl-9 col-lg-9 col-m-9 col-sm-12 col-12">
-            <div className="">
-              <div className="container ">
-                {/* <CarouselSlider /> */}
-                <div className="mt-5 row align-items-start justify-content-center">
-                  {courses.map((e, i) => (
-                    <div className="col-xl-4 col-lg-6 col-md-6 col-sm-8 col-12 justify-content-center d-flex mb-5  ">
-                      <div className="mb-5 col-xl-4 col-lg-6 col-md-6 col-sm-8 col-12 justify-content-center d-flex ">
-                        <DefaultCardWrapper>
-                          <img
-                            className="img-fluid"
-                            src={e.gallery.url}
-                            alt="rasm"
-                          />
-                          <div className="pe-5">
-                            <h4 className="mt-2">Имя: {e.fullName}</h4>
-                            <p className="subtitle">
-                              {e.courses[0].description}
-                            </p>
-                          </div>
-                          <div className="line"></div>
-                          <DefaultButton title={"Подробнее"} id={e.code} />
-                        </DefaultCardWrapper>
-                      </div>
-
-                      <DefaultCard
-                        code={e.id}
-                        key={e.id}
-                        img={e.gallery.url}
-                        subtitle={e.description}
-                        title={e.name}
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
+          <div className='mt-4 col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12'>
+            <div className="items-center justify-between mt-5 row">
+              {
+                courses.map((e, i) => <DefaultExpertCard code={e.code} key={e.id} subtitle={e.courses[0].name} img={e.gallery.url} title={e.fullName} />)
+              }
             </div>
           </div>
         </div>
