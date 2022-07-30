096af33b7a35cdb0969b37a80b083c52949b50ed
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-07-30 16:44:11 +0500

    master

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './i18n'
 import 'antd/dist/antd.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
