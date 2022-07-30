380b1946d753d1b1ad2e0cfab415bd06ed705675
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-07-30 19:22:05 +0500

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,96 +16,101 @@ import DefaultAllCourseButton from "../../components/DefaultAllCourseButton";
 import Statistics from "../../components/Statistics";
 import HomeServise from "../../components/HomeServise";
 import { instance } from "../../redux/actions";
-import FAQ from '../../components/FAQ'
+import FAQ from "../../components/FAQ";
 import { Spin } from "antd";
 export default function Home() {
-   const { t, i18n } = useTranslation();
-   const [courses, setCourses] = useState([])
-   const [loading, setLoading] = useState(false)
-   const getCourses = () => {
-      setLoading(true)
-      instance.get("api/v1/course/list/?size=10&page=0").then((res) => {
-         console.log(res.data.body);
-         setCourses([...res.data.body])
-         setLoading(false)
-      }).catch((err) => {
-         console.log(err);
+  const { t, i18n } = useTranslation();
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getCourses = () => {
+    setLoading(true);
+    instance
+      .get("api/v1/course/list/?size=10&page=0")
+      .then((res) => {
+        console.log(res.data.body);
+        setCourses([...res.data.body]);
+        setLoading(false);
       })
-         .catch((err) => {
-            console.log(err);
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   };
+      .catch((err) => {
+        console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-
-   useEffect(() => {
-      getCourses()
-   }, [])
-   return (
-      <Spin spinning={loading}>
-         <section>
-            <Carousel />
-         </section>
-         <section>
-            <CourseKadr />
-         </section>
-         <section>
-            <Statistics />
-         </section>
-         <section className="container px-5">
-            <HomeServise />
-         </section>
-         <section className="mt-5">
-            <div className="container px-5 py-5 bgCourse" style={{
-               background: "#F3F3FB",
-               borderRadius: "40px"
-            }}>
-               <div className="px-3 row align-items-center">
-                  <div className="col-xl-7 col-lg-6 col-md-6 col-sm-12 col-12">
-                     <TitleH1 title={t("courseTitleH1")} />
-                     <div className="mt-3">
-                        <DefaultAllCourseButton title={t("allcourse")} />
-                     </div>
-                  </div>
-                  <div className="col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12">
-                     <img className="img-fluid" src={undraw} alt="" />
-                  </div>
-               </div>
-               <div className="px-3 mt-5 row">
-                  {courses.map((e, i) => (
-                     <>
-                        <DefaultCard
-                           key={e.id}
-                           title={e.name}
-                           subtitle={e.decription}
-                           img={e.galleries[0].url}
-                        />
-                     </>
-                  ))}
-               </div>
+  useEffect(() => {
+    getCourses();
+  }, []);
+  return (
+    <Spin spinning={loading}>
+      <section>
+        <Carousel />
+      </section>
+      <section>
+        <CourseKadr />
+      </section>
+      <section>
+        <Statistics />
+      </section>
+      <section className="container px-5">
+        <HomeServise />
+      </section>
+      <section className="mt-5">
+        <div
+          className="container px-5 py-5 bgCourse"
+          style={{
+            background: "#F3F3FB",
+            borderRadius: "40px",
+          }}
+        >
+          <div className="px-3 row align-items-center">
+            <div className="col-xl-7 col-lg-6 col-md-6 col-sm-12 col-12">
+              <TitleH1 title={t("courseTitleH1")} />
+              <div className="mt-3">
+                <DefaultAllCourseButton title={t("allcourse")} />
+              </div>
             </div>
-         </section>
-         <section id="partnership">
-            <Partnership />
-         </section>
-         <section id="contactus" className="p-5">
-            <ContactWithUs />
-         </section>
-         <section className="p-5">
-            <div className="container px-4">
-               <div className="row justify-content-center">
-                  <div className="col-9 ">
-                     <TitleH1 title={t("faq")} />
-                     <FAQ />
-                  </div>
-               </div>
+            <div className="col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12">
+              <img className="img-fluid" src={undraw} alt="" />
             </div>
-         </section>
-         <section className="mt-5">
-            <Footer />
-         </section>
-      </Spin>
-   );
+          </div>
+          <div className="px-3 mt-5 row">
+            {courses.map((e, i) => (
+              <>
+                <DefaultCard
+                  key={e.id}
+                  title={e.name}
+                  subtitle={e.decription}
+                  img={e.galleries[0].url}
+                />
+              </>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="partnership">
+        <Partnership />
+      </section>
+      <section id="contactus" className="p-5">
+        <ContactWithUs />
+      </section>
+      <section className="p-5">
+        <div className="container px-4">
+          <div className="row justify-content-center">
+            <div className="col-9 ">
+              <TitleH1 title={t("faq")} />
+              <FAQ />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-5">
+        <Footer />
+      </section>
+    </Spin>
+  );
 }
