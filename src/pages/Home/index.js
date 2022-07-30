e361e7909298400d838e38f662a26fa4e7dfbe82
Javohir Toshmurodov <87381120+javohirToshmurodov@users.noreply.github.com>
2022-07-30 18:33:05 +0500

    master

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -73,7 +73,7 @@ export default function Home() {
                      <img className="img-fluid" src={undraw} alt="" />
                   </div>
                </div>
-               <div className="px-3 mt-5 row">
+               <div className="px-3 mt-5 row align-items-start">
                   {courses.map((e, i) => (
                      <>
                         <DefaultCard
